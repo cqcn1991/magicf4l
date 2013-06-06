f8605feee55687a45c68352936d352981aac0420
cqcn1991 <dadazhu_ashi@163.com>
2013-06-06 21:26:43 +0800

    a final guess on heroku and imgkit

--- config/initializers/imgkit.rb
+++ config/initializers/imgkit.rb
@@ -1,4 +1,4 @@
 IMGKit.configure do |config|
-  config.wkhtmltoimage = 'C:/wkhtmltopdf/wkhtmltoimage.exe'
+  #config.wkhtmltoimage = 'C:/wkhtmltopdf/wkhtmltoimage.exe'
   config.wkhtmltoimage = Rails.root.join('bin', 'wkhtmltoimage-amd64').to_s if ENV['RACK_ENV'] == 'production'
 end
