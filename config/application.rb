4ae8f62fa1e12b97e35bdcb778bdd8c7d15a9e56
cqcn1991 <dadazhu_ashi@163.com>
2013-07-22 23:24:53 +0800

    UI improved

--- config/application.rb
+++ config/application.rb
@@ -42,6 +42,7 @@ module Shuffle
     # Configure the default encoding used in templates for Ruby 1.9.
     config.encoding = "utf-8"
 
+
     # Configure sensitive parameters which will be filtered from the log file.
     config.filter_parameters += [:password]
 
@@ -66,6 +67,8 @@ module Shuffle
     config.assets.version = '1.0'
 
     config.middleware.use "PDFKit::Middleware"
+
+    config.assets.paths << Rails.root.join("app", "assets", "fonts")
 
     config.assets.initialize_on_precompile = false
   end
